057b4b21495d2b1981edf4f074283737fac853c4
DataNick <DataNick@users.noreply.github.com>
2016-02-06 16:52:04 -0500

    Removed sessions controller

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,8 @@ class Review
   include Mongoid::Document
   field :comment, type: String
 
+  validates :comment, :presence => true
+
   embedded_in :product, inverse_of: :reviews
   belongs_to :user
 end
