b5869dfa3fef2b89c730e7c3c94dd17bda7bd518
DataNick <DataNick@users.noreply.github.com>
2016-02-05 20:39:50 -0500

    Changed review object to review_params for create action and substituted create! for build

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,11 +2,15 @@ class ReviewsController < ApplicationController
   before_filter :load_product
 
   def create
-    @review = @product.reviews.build(params[:review])
+    @review = @product.reviews.create!(review_params)
     redirect_to @product, notice: "Review was created."
   end
 
   private
+
+  def review_params
+    params.require(:review).permit(:comment)
+  end
 
   def load_product
     @product = Product.find(params[:product_id])
