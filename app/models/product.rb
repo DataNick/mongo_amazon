0cf5392242ce0f5ff98def7a1c0a5dec38edb2b3
DataNick <DataNick@users.noreply.github.com>
2016-04-12 12:26:33 -0400

    Included mongoid-paperclip functionality

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,11 +1,17 @@
 class Product
   include Mongoid::Document
+  include Mongoid::Paperclip
+
   field :name, type: String
   field :description, type: String
   field :price_in_cents, type: Integer
 
   validates :description, :name, presence: true
   validates :price_in_cents, numericality: {only_integer: true}
+
+  has_mongoid_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  validates_attachment_content_type :avatar, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+
 
   embeds_many :reviews
   embeds_one :category
